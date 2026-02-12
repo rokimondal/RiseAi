35b84ba04f659dceb055954b051be3440fab44c9
ROKI MONDAL <rokimondal@ROKIs-MacBook-Air.local>
2026-02-13 00:25:04 +0530

    update favicon

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,9 @@ const inter = Inter({
 export const metadata = {
   title: "RiseAI",
   description: "AI powered assistant for your carreer growth",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
